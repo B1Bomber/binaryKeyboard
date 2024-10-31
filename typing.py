69113868a1c8df524169daf9e243e0059f820c25
B1Bomber <97653406+B1Bomber@users.noreply.github.com>
2024-10-30 20:00:32 -0700

    Update typing.py

--- typing.py
+++ typing.py
@@ -2,29 +2,25 @@ import serial
 import time
 #import pyautogui
 
-def binary_to_ascii(binary_value):
-    if len(binary_value) != 8:
-        raise Error("Input must be a 8-bit binary string");
-    
-    decimal_value = int(binary_value,2);
-
-    ascii_value = chr(decimal_value);
-
-    return ascii_value;
-
 #arduino = serial.Serial(port = 'COM7', baudrate = 9600, timeout = 1) 
-serialcomm = serial.Serial('/dev/cu.usbmodem101', 9600)
+serialcomm = serial.Serial('/dev/cu.usbserial-1120', 9600)
 serialcomm.timeout = 1
 
-while True:
-    i = input("input (on/ff): ").strip()
-    if i == 'done':
-        print ("finished program")
-        break 
-    serialcomm.write(i.encode())
-    time.sleep(0.5)
-    print(serialcomm.readline().decode('ascii'))
+def binary_to_ascii(binary_value):
+    if len(binary_value) != 8:
+        raise Error("Input must be a 8-bit binary string")
+    
+    decimal_value = int(binary_value,2)
+
+    ascii_value = chr(decimal_value)
+
+    return ascii_value
+
+
+#serialcomm.write(i.encode())
+#time.sleep(0.5)
+fromSerial = serialcomm.readline().decode('ascii')
 
 serialcomm.close
 
-pyautogui.write('Hello world!', interval=0.25)  # type with quarter-second pause in between each key
+#pyautogui.write('Hello world!', interval=0.25)  # type with quarter-second pause in between each key
