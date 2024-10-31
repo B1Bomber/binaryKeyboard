fadc4c5d79115af41f6a4726df2617c4121bb3ef
Ferrari Guan <ferrariguan@HP-Laptop-15-MacOS.local>
2024-10-30 20:20:59 -0700

    added the main function

--- typing.py
+++ typing.py
@@ -4,6 +4,7 @@ import time
 
 #arduino = serial.Serial(port = 'COM7', baudrate = 9600, timeout = 1) 
 serialcomm = serial.Serial('/dev/cu.usbserial-1120', 9600)
+#put your usb port as the first argument
 serialcomm.timeout = 1
 
 def binary_to_ascii(binary_value):
@@ -16,11 +17,20 @@ def binary_to_ascii(binary_value):
 
     return ascii_value
 
+def main():
+    fromSerial = serialcomm.readline().decode('ascii')
+    serialcomm.close
+
+    if fromSerial == "star":
+        # for switching to osu keyboard down the line
+        return
+    
+    #if fromSerial == "backspace":
+        #for doing a backspace maybe? Only if ascii doesn't have a backspace key
+
+    asciiKey = binary_to_ascii(int(fromSerial))
+    pyautogui.write('Hello world!', interval=0.25)  # type with quarter-second pause in between each key
+    #type the asciikey
 
 #serialcomm.write(i.encode())
 #time.sleep(0.5)
-fromSerial = serialcomm.readline().decode('ascii')
-
-serialcomm.close
-
-#pyautogui.write('Hello world!', interval=0.25)  # type with quarter-second pause in between each key
