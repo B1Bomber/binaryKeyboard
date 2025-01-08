f4382113c5066b7e7288d4f69b6e2e8d45da7af6
B2Bomber2 <f1guan@ucsd.edu>
2025-01-07 20:12:06 -0800

    created the final python file typer.py

---
+++ typer.py
@@ -0,0 +1,37 @@
+import serial
+import time
+import pyautogui
+
+pyautogui.FAILSAFE = True
+# ensures that moving mouse to the upper-left will exit program
+
+serialcomm = serial.Serial('/dev/cu.usbserial-1120', 9600)
+# put your usb port as the first argument
+
+serialcomm.timeout = 1
+
+def main():
+    fromSerial = serialcomm.readline().decode('ascii')
+    serialcomm.close
+
+    if len(fromSerial) < 8:
+        raise Error("Input must be a 8-bit binary string")
+
+    if fromSerial == "starOn":
+        # for switching to osu keyboard down the line
+        return
+    
+    if fromSerial == "starOff":
+        # for switching back to binary keyboard down the line
+        return
+    
+    
+    
+    decimal_value = int(fromSerial, 2) 
+    asciiKey = chr(decimal_value)
+
+    pyautogui.write(asciiKey, interval=0.25)
+    # type the asciiKey with quarter-second pause in between each key
+
+    main()
+    # repeatedly run this because someone will be typing something. 
