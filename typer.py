1b50fcdd3912b64b48ac268a7fe7e453de4da0ad
B2Bomber2 <f1guan@ucsd.edu>
2025-01-14 11:22:25 -0800

    Fixed the typing delay

--- typer.py
+++ typer.py
@@ -9,7 +9,7 @@ pyautogui.FAILSAFE = True
 # put your usb port as the first argument
 # put your baud rate as the second argument
 
-serialcomm = serial.Serial('/dev/cu.usbserial-10', 9600, timeout=1, rtscts=True)
+serialcomm = serial.Serial('/dev/cu.usbserial-110', 9600, timeout=1, rtscts=True)
 
 def main():
     fromSerial = serialcomm.readline().decode('ascii').strip()
@@ -29,7 +29,7 @@ def main():
 
         print(asciiKey)
 
-        time.sleep(1)
+        time.sleep(0.5)
         pyautogui.typewrite(asciiKey, interval=0.25)
         # type the asciiKey with quarter-second pause in between each key
     
