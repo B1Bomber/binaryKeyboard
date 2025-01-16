626a1c089e0993e4a43d407513d3eb7e49ee400d
B2Bomber2 <156377868+B2Bomber2@users.noreply.github.com>
2025-01-16 11:24:46 -0800

    Update binary_keyboard_OLED.ino

--- binary_keyboard_OLED/binary_keyboard_OLED.ino
+++ binary_keyboard_OLED/binary_keyboard_OLED.ino
@@ -1,126 +1,198 @@
-#include <Wire.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
-#include "U8x8lib.h"
+#include <Wire.h>
+#include <SPI.h>
+// #include "U8x8lib.h"
 
-#define OLED_WIDTH 128
-#define OLED_HEIGHT 64
+const int OLED_WIDTH = 128;
+const int OLED_HEIGHT = 64;
+const int reset = -1;
 
 #define OLED_ADDR   0x3C
 
-Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT);
+Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT, &Wire, reset);
 
-int zero = 12;
-int one = 11;
-int enter = 10;
+const int zeroButton = 12;
+const int oneButton = 11;
+const int enterButton = 10;
 
-int col = 1;
+int displayCursorColumn = 12;
+// increases by 12
 
-char binary[8] = {};
-char (*previousBinary)[8] = &binary;
-//char previousBinary[8] = {};
-int index = 0;
+char binary[9];
+int binaryIndex = 0;
 
-void setup() {
-  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
-  display.clearDisplay();
+const int keyInputDelay = 350;
 
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println("Welcome to PIB Binary Keyboard");
-  display.display(); 
-  //may need to this function
-  
-  pinMode(zero, INPUT_PULLUP);
-  pinMode(one, INPUT_PULLUP);
-  pinMode(enter, INPUT_PULLUP);
-  
-  //debug
-  Serial.begin(800);
+bool osuKeyboard = false;
+const int osuKeyInputDelay = 250;
+
+void setup(){
+    Serial.begin(9600);
+
+    pinMode(zeroButton, INPUT_PULLUP);
+    pinMode(oneButton, INPUT_PULLUP);
+    pinMode(enterButton, INPUT_PULLUP);
+
+    display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+    display.display();
+    delay(2000);
+    display.clearDisplay();
+
+    startText();
 }
 
 void loop(){
-  int index = col - 1;
-  
-  //debug
-  //Serial.println(digitalRead(zero));
-  //Serial.println(col);
-  
-  //debug binary conversion
-  //Serial.println(binary[0]);
-  //Serial.println(binary[1]);
-  //Serial.println(binary[2]);
-  //Serial.println(binary[3]);
-  //Serial.println(binary[4]);
-  //Serial.println(binary[5]);
-  //Serial.println(binary[6]);
-  //Serial.println(binary[7]);
-  
-  if (col == 9){
-    if ((digitalRead(zero) == 0) || (digitalRead(one) == 0)){
-      enterFunction();
+    while (binaryIndex < 9){
+        if (digitalRead(zeroButton) == LOW && digitalRead(oneButton) == HIGH && digitalRead(enterButton) == HIGH){
+            pressZero();
+            delay(keyInputDelay);
+        }
+        else if (digitalRead(oneButton) == LOW && digitalRead(zeroButton) == HIGH && digitalRead(enterButton) == HIGH){
+            pressOne();
+            delay(keyInputDelay);
+        }
+        else if (digitalRead(enterButton) == LOW && digitalRead(zeroButton) == HIGH && digitalRead(oneButton) == HIGH){
+            pressEnter();
+            delay(keyInputDelay);
+        }
+        else if (digitalRead(zeroButton) == LOW && digitalRead(enterButton) == LOW && digitalRead(oneButton) == HIGH){
+            osuKeyboard = true;
+            Serial.println("starOn");
+            changeKeyboard(osuKeyboard);
+            delay(500);
+            pressClear();
+            refillDisplay();
+        }
+        else if (digitalRead(oneButton) == LOW && digitalRead(enterButton) == LOW && digitalRead(zeroButton) == HIGH){
+            pressBack();
+            delay(keyInputDelay);
+        }
+        else if (digitalRead(zeroButton) == LOW && digitalRead(oneButton) == LOW && digitalRead(enterButton) == LOW){
+            pressClear();
+            delay(keyInputDelay);
+        }
     }
-    else if (digitalRead(enter) == 0){
-      enterFunction();
-    }
-    //else other stuff?
-  }
-  else{
-    binaryKeb();
-  }
-  
+
+
+    pressEnter();
+
 }
 
-void enterFunction(){
-  //copy binary to previousBinary
-  char (*previousBinary)[8] = &binary;
-  
-  //conversion
-  atoi(binary);
-  // have python do this instead
+void startText(){
+    const char startTextLines[4][10] = {"Welcome to", "PIB Binary", "Keyboard! ", "<________>"};
+    const int length = sizeof(startTextLines) / sizeof(startTextLines[0]);
 
-  //debug
-  Serial.println(binary);
-
-  int binary[8] = {};
-  //lcd.clear();
-  col = 1;
-  display.setCursor(index, 0);
-  display.print("[________]");
-  display.setCursor(col, 0);
+    for (int i = 0; i < length; i++){
+        display.clearDisplay();
+        display.setTextSize(2);
+        display.setTextColor(WHITE);
+        display.setCursor(0, 0);
+        display.println(startTextLines[i]);
+        delay(2000);
+        display.display();
+    }
 }
 
-void binaryKeb(){
-  if (digitalRead(zero) == 0){
-   display.setCursor(col, 0);
-   display.print("0");
-   col = col + 1;
-   int index = col - 1;
-   binary[index] = "0";
-  }
-  else if (digitalRead(one) == 0){
-    display.setCursor(col, 0);
-    display.print("1");
-    col = col + 1;
-    int index = col - 1;
-    binary[index] = "1";
-  }
-  else if (digitalRead(enter) == 0){
-    enterFunction();
-  }
-  else if ((digitalRead(zero) == 0) && (digitalRead(enter) == 0)){
-    display.print("_");
-    binary[index - 1] = 0;
-  }
-  else if ((digitalRead(one) == 0) && (digitalRead(enter) == 0)){
+void pressZero(){
+    displayPress('0');
+    
+    binary[binaryIndex] = '0';
+
+    binaryIndex += 1;
+    displayCursorColumn += 12;
+
+    Serial.println("Pressed 0");
+}
+
+void pressOne(){
+    displayPress('1');
+    binary[binaryIndex] = '1';
+
+    binaryIndex += 1;
+    displayCursorColumn += 12;
+
+    Serial.println("Pressed 1");
+}
+
+void pressEnter(){
+    if (binaryIndex == 8){
+        Serial.println(binary);
+    }
+
+    pressClear();
+}
+
+void pressBack(){
+    display.fillRect(displayCursorColumn, 0, 12, 16, BLACK);
+    display.display();
+    binary[binaryIndex] = NULL;
+}
+
+void pressClear(){
     display.clearDisplay();
-    col = 1;
-    display.print("[________]");
-  }
-  //else{
-    // all 3 are pressed
-  //return to previousBinary
-  //}
-  delay(100);
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.println("<________>");
+    display.display();
+
+    displayCursorColumn = 12;
+
+    binary[9] = {};
+    binary[8] = '\0';
+
+    binaryIndex = 0;
+}
+
+void changeKeyboard(bool & keyboardOSU){
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.println("OSU! MODE");
+    display.display();
+
+    while (keyboardOSU == true){
+        if (digitalRead(zeroButton) == LOW){
+            // press z key
+            Serial.println("Z");
+            delay(osuKeyInputDelay);
+        }
+        else if (digitalRead(oneButton) == LOW){
+            // press x key
+            Serial.println("X");
+            delay(osuKeyInputDelay);
+        }
+        else if (digitalRead(enterButton) == LOW){
+            // put the previous entered bits in
+            keyboardOSU = false;
+            Serial.println("starOff");
+            delay(350);
+        }
+        else if (digitalRead(zeroButton) == LOW && digitalRead(enterButton) == LOW){
+            // put the previous entered bits in
+            keyboardOSU = false;
+            Serial.println("starOff");
+            delay(350);
+        }
+    }
+}
+
+void displayPress(char input){
+    display.setCursor(displayCursorColumn, 0);
+    display.fillRect(displayCursorColumn, 0, 12, 16, BLACK);
+    display.print(input);
+    display.display();
+}
+
+void refillDisplay(){
+    for (int i = 0; i < 9; i++){
+        if (binary[i] == '0'){
+            pressZero();
+        }
+        else if (binary[i] == '1'){
+            pressOne();
+        }
+    }
 }
